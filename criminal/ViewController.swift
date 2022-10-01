9bb2d95bd2b79e938b56f6d9f1914989f8332b9e
tuffdeblain <himanfullhd@gmail.com>
2022-10-01 21:22:04 +0700

    Initial Commit

---
+++ criminal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  criminal
+//
+//  Created by Сергей Кудинов on 01.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
